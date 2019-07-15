5549061ffe3071420804181c2d16de5ac1d661f0
THitokuse <hasegawa.taichiro@gmail.com>
2019-07-16 06:51:50 +0900

    JWTの複合

--- mediaserver/app/controllers/VideoController.scala
+++ mediaserver/app/controllers/VideoController.scala
@@ -3,14 +3,37 @@ package controllers
 import javax.inject._
 import play.api.Configuration
 import play.api.mvc._
+import play.api.libs.json.Json
+
+import pdi.jwt.{Jwt, JwtAlgorithm}
+
+import scala.util.Success
 import scala.concurrent.Future
 
 @Singleton
 class VideosController @Inject()(cc: ControllerComponents,
                                  configuration: Configuration) extends AbstractController(cc) {
 
+  val secret = configuration.get[String]("nnDouga.secret")
+
+  val originalStorePath = configuration.get[String]("nnDouga.filesystem.original")
+
   def post() = Action.async { implicit request: Request[AnyContent] =>
-    Future.successful(Ok("File posted."))
+    request.body.asMultipartFormData match {
+      case Some(form) =>
+        (form.file("file"), form.dataParts.get("apiToken")) match {
+          case (Some(file), Some(Seq(apiToken))) =>
+            val decoded = Jwt.decodeRawAll(apiToken, secret, Seq(JwtAlgorithm.HS256))
+            (file.contentType, decoded) match {
+              case (Some(ct), Success((_, jsonString, _))) =>
+                val json = Json.parse(jsonString)
+                val userId = (json \ "userId").validate[Long].get
+                val expire = (json \ "expire").validate[Long].get
+                Future.successful(Ok(s"userId:${userId}, expire:${expire}"))
+            }
+        }
+      case _ => Future.successful(BadRequest("Need form data."))
+    }
   }
 
 }
