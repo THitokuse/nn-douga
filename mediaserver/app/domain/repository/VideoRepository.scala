45e4f81d68364125e80f1347d593f8421696d965
THitokuse <hasegawa.taichiro@gmail.com>
2019-07-18 03:07:34 +0900

    動画をvideoテーブルに保存できるように設定

---
+++ mediaserver/app/domain/repository/VideoRepository.scala
@@ -0,0 +1,16 @@
+package domain.repository
+
+import domain.entity.Video
+
+import scala.concurrent.Future
+
+trait VideoRepository {
+
+  /**
+    * 動画のプロパティを保存する
+    *
+    * @param video 保存する動画のプロパティ値
+    */
+  def create(video: Video): Future[Unit]
+
+}
