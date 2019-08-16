5509c5a7d72874f56b16dfac64501d941584ad64
THitokuse <hasegawa.taichiro@gmail.com>
2019-08-17 01:19:02 +0900

    自分の動画投稿一覧画面の作成

--- webserver/webfront/main.js
+++ webserver/webfront/main.js
@@ -5,6 +5,7 @@ import * as Pixi from 'pixi.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Uploader from './uploader.js';
+import MyVideoTable from './myVideoTable.js';
 
 const uploaderDivided = document.getElementById('dropbox-container');
 if (uploaderDivided) {
@@ -14,5 +15,13 @@ if (uploaderDivided) {
       mediaserverUrlRoot={uploaderDivided.dataset.mediaserverUrlRoot}
     />,
     uploaderDivided
+  );
+}
+
+const myVideoTableDivided = document.getElementById('my-video-table');
+if (myVideoTableDivided) {
+  ReactDOM.render(
+    <MyVideoTable apiToken={myVideoTableDivided.dataset.apiToken} />,
+    myVideoTableDivided
   );
 }
